2318ea312ef101ce2af345db55976b61b364d23f
kamily <kamily7803@gmail.com>
2024-03-18 08:50:06 -0300

    feat: new tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-black': '#1E1E1E',
+        'custom-yellow': '#FFBB00',
+        'custom-light-gray': '#EFEFEF',
+        'custom-dark-gray': '#C5C5C5',
+      },
+    },
   },
   variants: {
     extend: {},
