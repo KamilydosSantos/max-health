e3d72ccff5b4b529a3c93b03d806e75c7e9188ca
kamily <kamily7803@gmail.com>
2024-03-18 10:17:05 -0300

    feat: add responsive navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,50 +1,55 @@
-
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
 
 </script>
 
 <template>
-  <div class="flex items-center bg-black p-8 w-full">
-    <nav class="flex items-center">
-      <div class="flex items-center gap-4 text-yellow-400 text-2xl font-bold">
-        <router-link to="/"><img src="../assets/max_health-logo.svg" alt=""></router-link>
-        <span>MaxHealth</span>
-      </div>
-      <router-link to="/">Início</router-link>
-      <router-link to="/sobre">Sobre Nós</router-link>
-      <router-link to="/blog">Blog</router-link>
-      <router-link to="/atividades">Atividades</router-link>
-      <router-link to="/contato">Contato</router-link>
-    </nav>
-    <button class="mobileButton"></button>
+  <div class="flex items-center justify-center bg-black p-4 w-full">
+      <nav class="flex items-center justify-between max-w-screen-lg w-full">
+        <div class="flex items-center gap-4 text-custom-yellow text-2xl font-bold">
+          <router-link to="/"><img src="../assets/max_health-logo.svg" alt=""></router-link>
+          <span>MaxHealth</span>
+        </div>
+        <div class="flex items-center gap-8">
+          <router-link class="text-white hidden lg:flex" to="/">Início</router-link>
+          <router-link class="text-white hidden lg:flex" to="/sobre">Sobre Nós</router-link>
+          <router-link class="text-white hidden lg:flex" to="/blog">Blog</router-link>
+          <router-link class="text-white hidden lg:flex" to="/atividades">Atividades</router-link>
+          <router-link class="text-white hidden lg:flex" to="/contato">Contato</router-link>
+        </div>
+      </nav>
+      <button class="mobileButton flex lg:hidden"></button>
   </div>
 </template>
 
 <style scoped lang="scss">
 .mobileButton {
   position: relative;
-  background: white;
+  background: transparent;
   border-radius: .2rem;
   height: 40px;
   width: 40px;
-  display: flex;
   align-items: center;
   justify-content: center;
   border: 1px solid transparent;
   transition: .1s;
   cursor: pointer;
+  &::after {
+    content: '';
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 20px;
+    height: 2px;
+    background: #fb0;
+    box-shadow: 0 6px #fb0, 0 -6px #fb0;
+  }
+  &:hover,
+  &:focus {
+    outline: none;
+    border-color: #fb0;
+  }
 }
 
-.mobileButton::after {
-  content: '';
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  width: 20px;
-  height: 2px;
-  background: red;
-  box-shadow: 0 6px red, 0 -6px red;
-}
 </style>
