378a91709ea76889104406677d56923da1ea073c
kamily <kamily7803@gmail.com>
2024-03-20 10:45:24 -0300

    feat: add initial activities component

---
+++ src/components/InitialActivities.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Title from './Title.vue';
+import NavigateButton from './NavigateButton.vue';
+
+</script>
+
+<template>
+  <div class="grid grid-rows-[1ft,auto] lg:grid-cols-2 gap-8 items-center m-8 lg:mx-auto max-w-screen-lg">
+    <div>
+      <img class="w-full" src="../assets/mh-activities.png" alt="">
+    </div>
+    <div class="flex flex-col gap-8 h-max text-custom-black">
+      <Title text="Uma variedade de atividades e exercícios" />
+      <p>Oferecemos uma ampla gama de modalidades de exercícios e atividades para atender às preferências e necessidades dos nossos clientes. Além da possibilidade de serviços de treinamento pessoal individualizado e programas de exercícios adaptados às suas metas e habilidades específicas.</p>
+      <NavigateButton text="Nossas Atividades" route="/atividades" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import InitialAbout from '../components/InitialAbout.vue';
+import InitialActivities from '../components/InitialActivities.vue';
 import PrincipalHeader from '../components/PrincipalHeader.vue';
 </script>
 
 <template>
   <div class="flex flex-col gap-12 lg:gap-24">
     <PrincipalHeader/>
-    <main>
+    <main class="flex flex-col gap-12 lg:gap-24">
       <InitialAbout />
+      <InitialActivities />
     </main>
   </div>
 </template>
