a0ffb8e743d471de194e7338383deac8a8536142
kamily <kamily7803@gmail.com>
2024-04-05 17:56:11 -0300

    feat: add blog component view and fetch blog data

---
+++ src/components/Curiosities.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{ curiosities[0].title }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue';
+
+interface Curiosity {
+  id: number;
+  title: string;
+  text: string;
+}
+
+export default {
+  props: {
+    curiosities: {
+      type: Array as PropType<Curiosity[]>,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,12 +1,85 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div>
-    <h1>Blog</h1>
+    <Curiosities :curiosities="curiosities" />
   </div>
 </template>
 
-<style lang="scss">
+<script lang="ts">
+import Curiosities from '../components/Curiosities.vue';
+import { ref, onMounted } from 'vue';
+import { api } from '../services';
 
+interface Blog {
+  initial: Initial[];
+  curiosities: Curiosity[];
+  events: Event[];
+  recommendations: Recommendation[];
+  news: New[];
+}
+
+interface Initial {
+  id: number;
+  icon: string;
+  text: string;
+}
+
+interface Curiosity {
+  id: number;
+  title: string;
+  text: string;
+}
+
+interface Event {
+  id: number;
+  date: string;
+  name: string;
+}
+
+interface Recommendation {
+  id: number;
+  title: string;
+  text: string;
+}
+
+interface New {
+  id: number;
+  title: string;
+  phrase: string;
+  text: string;
+}
+
+export default {
+  components: {
+    Curiosities
+  },
+  setup() {
+      const curiosities = ref<Curiosity[]>([]);
+      const events = ref<Event[]>([]);
+      const recommendations = ref<Recommendation[]>([]);
+      const news = ref<New[]>([]);
+      onMounted(async () => {
+          try {
+            const data = await api.get<Blog>('/blog');
+            if (data) {
+              curiosities.value = data.curiosities || [];
+              events.value = data.events || [];
+              recommendations.value = data.recommendations || [];
+              news.value = data.news || [];
+            }
+          }
+          catch (error) {
+              console.error('Erro ao buscar dados:', error);
+          }
+      });
+      return {
+          curiosities,
+          events,
+          recommendations,
+          news
+      };
+  }
+}
+</script>
+  
+<style lang="scss">
 </style>
